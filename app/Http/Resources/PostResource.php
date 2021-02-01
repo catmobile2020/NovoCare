1fb58d213d58240de5ac98de0fc497baaaa7b8d4
mo7amad7amdy <mohammad-hamdy@hotmail.com>
2021-02-01 12:14:15 +0200

    edit image url api and add image lib to composer

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -9,6 +9,15 @@ class PostResource extends JsonResource
 {
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'            => $this->id,
+            'title'         => $this->title,
+            'slug'          => $this->slug,
+            'caption'       => $this->caption,
+            'image'         => asset($this->image),
+            'text'          => $this->text,
+            'created_at'    => (string)$this->created_at,
+            'updated_at'    => (string)$this->updated_at
+        ];
     }
 }
