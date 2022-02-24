6415fe5db2de1179b6a9ff2e8434dd77bf9f4962
MaymarHany <80677822+Maymar-hany@users.noreply.github.com>
2022-02-24 14:51:44 +0200

    update login-page-style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 <div class="container-fluid h-100 p-0">
     <div class="row justify-content-center no-gutters d-flex align-items-stretch h-100">
-    <div class="col-md-7 bg-login-register d-none d-md-block">
+    <div class="col-md-7 bg-login-register d-none d-md-block h-100">
+        <img class="w-100 h-100" src="{{ asset('img/img.png') }}" alt="">
+        <img class="img-fluid hotline" src="{{ asset('img/hotline.png') }}" alt="">
+
     </div>
         <div class="col-md-5">
-                <div class="card-body">
-                                      <h3 class="text-center text-dark"> Login Account</h3>
-                                      <hr>
+                <div class="conOfleftSide">
+                    <div class="conOflogo-img pb-3">
+                    <img class="img-fluid" src="{{ asset('img/logo.png') }}" alt="">
+                    </div>
+
+                                      <h3 class="text-center text-dark pb-3"> Login Account</h3>
                                     <form method="POST" action="{{ route('login') }}">
                                         @csrf
 
                                         <div class="form-group row d-flex justify-content-center">
-                                            <div class="col-md-6">
+                                            <div class="col-md-12">
                                                 <input id="email" type="email" placeholder="Email Address" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                                 @error('email')
@@ -23,7 +29,7 @@
                                         </div>
 
                                         <div class="form-group row d-flex justify-content-center">
-                                            <div class="col-md-6">
+                                            <div class="col-md-12">
                                                 <input id="password" type="password"  placeholder="Password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                                 @error('password')
@@ -35,7 +41,7 @@
                                         </div>
 
                                         <div class="form-group row  d-flex justify-content-center">
-                                            <div class="col-md-6">
+                                            <div class="col-md-12">
                                                 <div class="form-check">
                                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -45,10 +51,9 @@
                                                 </div>
                                             </div>
                                         </div>
-                                      <hr>
 
                                         <div class="form-group row mb-0 text-center d-flex justify-content-center">
-                                            <div class="col-md-6 text-center">
+                                            <div class="col-md-12 text-center">
                                                 <button type="submit" class="btn m-auto btn-primary w-100" style=" padding: 1rem; ">
                                                     {{ __('Login') }}
                                                 </button>
@@ -73,12 +78,30 @@
 @endsection
 
 <style>
+    .conOflogo-img{
+        width: 30%;
+    }
+    .conOfleftSide{
+        width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    align-items: center;
+}
+    }
     .bg-login-register {
         background-color: #4e73df;
             background-image: linear-gradient(
         180deg
         ,#4e73df 10%,#224abe 100%);
             background-size: cover;
+            position: relative;
+    }
+    .hotline{
+        position: absolute;
+        left: 2%;
+        bottom: 2%;
     }
     .card-body {
         padding-top: 30vh !important;
