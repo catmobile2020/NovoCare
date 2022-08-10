598ab7c31e8ca31764389517feb4a810beda1a50
Mohammed Hamdy <Mohammad-Hamdy@hotmail.com>
2022-08-10 11:11:52 +0200

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::resource('activities', \App\Http\Controllers\ActivityController::class);
 Route::resource('devices', \App\Http\Controllers\DeviceController::class);
 
 Route::get('/optimize/clear', function () {
-    dd(\App\Device::delete(), \App\Activity::delete());
+    dd(\App\Device::truncate(), \App\Activity::truncate());
     \Artisan::call('cache:clear');
     \Artisan::call('view:clear');
     \Artisan::call('route:clear');
